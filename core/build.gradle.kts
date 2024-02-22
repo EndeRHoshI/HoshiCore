d004a48074846fe868430be8ff8d901b20916464
lvqx <lvqixing@ddpai.com>
2024-02-22 11:40:50 +0800

    :wrench: 移除掉本地 maven 的相关代码

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("kotlin-android")
     id("maven-publish")
 }
-apply("../local-maven.gradle")
+// apply("../local-maven.gradle")
 
 android {
     sourceSets.getByName("main") {
@@ -43,6 +43,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.2") // 协程
 
     implementation("androidx.startup:startup-runtime:1.1.1") // 统一处理初始化
+    implementation("com.github.dcendents:android-maven-gradle-plugin:2.1") // jitpack 打包插件
 }
 
 // 需要这段代码才能推上仓库，可以看看具体是什么
