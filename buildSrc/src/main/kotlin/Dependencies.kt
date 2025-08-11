4eefd4a549a0f0d2a27d24447399ba04f1a988bf
lvqx <lvqixing@ddpai.com>
2025-08-11 17:37:19 +0800

    :rocket: 版本提升到 1.0.3

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
 object Versions {
     private const val compileAndTargetSdk = 35
-    const val coreVersion = "1.0.2.1" // 小版本迭代是四段式，正式稳定版是三段式，比如 1.0.2.1 是改 bug 或者调试，用稳定后改成 1.0.3
-    const val libName = "core"
+    const val coreVersion = "1.0.3" // 小版本迭代是四段式，正式稳定版是三段式，比如 1.0.2.1 是改 bug 或者调试，用稳定后改成 1.0.3
+    const val libName = "core" // 库的名称
     const val compileSdk = compileAndTargetSdk
     const val targetSdk = compileAndTargetSdk
     const val minSdk = 24
