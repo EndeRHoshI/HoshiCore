9a04b0562435fd2d13f120ee65bdd46389d31fbe
lvqx <lvqixing@ddpai.com>
2026-02-26 16:48:33 +0800

    :art: 在 app 模块中，通过 BuildConfig 显示当前版本号

--- app/src/main/kotlin/com/hoshi/core/MainActivity.kt
+++ app/src/main/kotlin/com/hoshi/core/MainActivity.kt
@@ -3,14 +3,15 @@ package com.hoshi.core
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.hoshi.core.databinding.ActivityMainBinding
+import com.hoshi.core.utils.SystemUtils
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+        binding.tvVersion.text = SystemUtils.getCoreVersion()
     }
 
 }
